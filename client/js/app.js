4f0b14175aab468aa887ed17bffc3644dbf75308
humikun <55233582+humikun@users.noreply.github.com>
2019-09-14 02:53:37 +0900

    Update app.js

--- client/js/app.js
+++ client/js/app.js
@@ -78,7 +78,7 @@ function showRRKDetails(rrkid) {
 }
 
 function getKBList(success, error) {
-  var soql = "SELECT Id, Name, CNN_RRK__c,KB_LN_SUB_NUM__c,CNN_RRK__c, CNN_RRK__.name FROM KOBAN__c";
+  var soql = "SELECT Id, Name, KB_LN_NUM__c, KB_LN_NUM__r.name FROM KOBAN__c";
   force.query(soql, success, error);
 }
 
